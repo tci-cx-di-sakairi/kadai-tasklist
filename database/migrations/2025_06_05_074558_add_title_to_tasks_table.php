42c505f7e559a9d0d292207d6c83883dbea5a583
shunsuke.sakairi <sakairi.shunsuke1@trans-cosmos.co.jp>
2025-06-09 11:34:09 +0900

    re:init "動かない不具合-依存回りを修正"

--- database/migrations/2025_06_05_074558_add_title_to_tasks_table.php
+++ database/migrations/2025_06_05_074558_add_title_to_tasks_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
     {
         Schema::table('tasks', function (Blueprint $table) {
             //
+            $table->dropColumn('status');
         });
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,18 @@ use App\Http\Controllers\TasksController;
 |
 */
 
+Route::get('/', function () {
+    return view('dashboard');
+});
 
-Route::get('/', [TasksController::class, 'index']);
-Route::resource('tasks', TasksController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
